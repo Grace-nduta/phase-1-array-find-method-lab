387a6706ec9c911797266701ee0090a86efba6a4
Grace Nduta <ndutak5674@gmail.com>
2025-03-11 19:54:11 +0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const recordWin= record.find(game => game.result === "W");
+    return recordWin ? recordWin.year : undefined;
+  }
+
+  console.log(recordWin)
